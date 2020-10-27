876070a333f5ceccd908bc877a7d612fe5effd05
Felipe Rodrigues <felipekjr@gmail.com>
2020-10-27 14:45:26 -0300

    Criar script para renderizar vídeos de forma automática em diferentes resoluções

---
+++ script.sh
@@ -0,0 +1,42 @@
+ASSETSFOLDER=assets/timeline
+for mediaFile in `ls $ASSETSFOLDER | grep .mp4`; do
+    # cortas a extensao e resolução do arquivo
+    FILENAME=$(echo $mediaFile | sed -n 's/.mp4//p' | sed -n 's/-1920x1080//p')
+    INPUT=$ASSETSFOLDER/$mediaFile
+    FOLDER_TARGET=$ASSETSFOLDER/$FILENAME
+    mkdir -p $FOLDER_TARGET
+
+    #criar arquivos de resoluções diferentes nas pastas
+    OUTPUT=$ASSETSFOLDER/$FILENAME/$FILENAME
+    DURATION=$(ffprobe -i $INPUT -show_format -v quiet | sed -n 's/duration=//p')
+   
+    OUTPUT720=$OUTPUT-$DURATION-720
+    OUTPUT360=$OUTPUT-$DURATION-360
+
+    echo 'rendering in 720p'
+    sudo ffmpeg -y -i $INPUT \
+        -c:a aac -ac 2 \
+        -vcodec h264 -acodec aac \
+        -ab 128k \
+        -movflags frag_keyframe+empty_moov+default_base_moof \
+        -b:v 1500k \
+        -maxrate 1500k \
+        -bufsize 1000k \
+        -vf "scale=-1:720" \
+        -v quiet \
+        $OUTPUT720.mp4
+
+    echo 'rendering in 360p'
+    sudo ffmpeg -y -i $INPUT \
+        -c:a aac -ac 2 \
+        -vcodec h264 -acodec aac \
+        -ab 128k \
+        -movflags frag_keyframe+empty_moov+default_base_moof \
+        -b:v 400k \
+        -maxrate 400k \
+        -bufsize 400k \
+        -vf "scale=-1:360" \
+        -v quiet \
+        $OUTPUT360.mp4
+    
+done
